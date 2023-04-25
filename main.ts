0ba06c5d165fbff1d8f782a0da4ef66799db0c8b
wyatthacks420 <122548054+wyatthacks420@users.noreply.github.com>
2023-04-25 11:25:03 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    action = receivedNumber
+})
+function forkDown () {
+    wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 0)
+}
+function reverse () {
+    wuKong.setAllMotor(-20, -20)
+}
+function forkUp () {
+    wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 60)
+}
+function moveFoward () {
+    wuKong.setAllMotor(20, 20)
+}
+let action = 0
+basic.showIcon(IconNames.Skull)
+joystickbit.initJoystickBit()
+radio.setGroup(21)
+basic.forever(function () {
+	
+})
