70499570da655c21a86b03b152cd323d59322d7d
wyatthacks420 <122548054+wyatthacks420@users.noreply.github.com>
2023-05-01 10:12:48 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -17,3 +17,33 @@ let action = 0
 basic.showIcon(IconNames.Skull)
 joystickbit.initJoystickBit()
 radio.setGroup(21)
+basic.forever(function () {
+    if (action == 1) {
+        moveFoward()
+    }
+    if (action == 2) {
+        reverse()
+    }
+    if (action == 6) {
+        forkUp()
+    }
+    if (action == 7) {
+        forkDown()
+    }
+    if (action == 0) {
+        wuKong.stopAllMotor()
+    }
+})
+basic.forever(function () {
+    if (joystickbit.getRockerValue(joystickbit.rockerType.Y) > 100) {
+        radio.sendNumber(1)
+    } else if (joystickbit.getRockerValue(joystickbit.rockerType.Y) < 200) {
+        radio.sendNumber(2)
+    } else if (joystickbit.getButton(joystickbit.JoystickBitPin.P13)) {
+        radio.sendNumber(6)
+    } else if (joystickbit.getButton(joystickbit.JoystickBitPin.P14)) {
+        radio.sendNumber(7)
+    } else {
+        radio.sendNumber(0)
+    }
+})
